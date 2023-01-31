74062151c60bd04b7e705ce53e78e48d1781a29a
luigicara <luigi.caravelli98@gmail.com>
2023-01-31 09:52:56 +0100

    header completed

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,31 @@
+@include('components.data');
+
+@php
+
+    $navItems = ['CHARACTHERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP'];
+@endphp
+
+
+
+<div class="container">
+    <header>
+        <img src={{ Vite::asset('/resources/img/dc-logo.png') }} alt="logo">
+    
+        <nav>  
+            <ul>
+                @php
+                    foreach ($navItems as $item)
+                        echo '<li><a href="">'
+                            . $item
+                            . "</a></li>"
+                @endphp
+            </ul>
+        </nav>
+
+    </header>
+</div>
+
+<section class="jumbotron">
+    <img src= {{ Vite::asset('/resources/img/jumbotron.png') }}   alt="jumbotron">
+</section>
+
